840f2e898c315d4dcefcc685e1bf67d9715e5e7a
kkuryshev <kkuryshev@tsconsulting.ru>
2020-04-26 15:00:11 +0300

    Добавил необходимые файлы для публикации на PyPi

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='mosenergosbyt',
+    packages=['mosenergosbyt'],
+    version='0.1',
+    license='MIT',
+    description='api для работы с порталом мосэнергосбыт',
+    author='@kkuryshev',
+    author_email='kkurishev@gmail.com',
+    url='https://github.com/kkuryshev/mosenergosbyt',
+    download_url='',
+    keywords=['mosenergosbyt', 'MEANINGFULL', 'KEYWORDS'],
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
